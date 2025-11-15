15c4550c34f487840096e707141fcfb3651a5639
Dreamer356 <glukhovsky.v@gmail.com>
2025-11-15 18:05:35 +0200

    +

--- src/components/ErrorMessage/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './ErrorMessage.module.css'
-
-interface ErrorMessageProps { message?: string }
-
-const ErrorMessage = ({ message = 'There was an error, please try again...' }: ErrorMessageProps) => {
-  return <p className={styles.text}>{message}</p>
-}
-
-export default ErrorMessage
--- src/components/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Loader.module.css'
-
-interface LoaderProps { message?: string }
-
-const Loader = ({ message = 'Loading movies, please wait...' }: LoaderProps) => {
-  return <p className={styles.text}>{message}</p>
-}
-
-export default Loader
