89084c41b621d4a44b520b0c6b6736cb4247b149
Dreamer356 <glukhovsky.v@gmail.com>
2025-11-17 11:39:27 +0200

    +

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
-import type { MoviesResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = "https://api.themoviedb.org/3";
+
+export interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 export async function fetchMovies(
   query: string,
   page: number
 ): Promise<MoviesResponse> {
-  const config = {
+  const response = await axios.get<MoviesResponse>(`${BASE_URL}/search/movie`, {
     params: {
       query,
       page,
@@ -18,10 +25,7 @@ export async function fetchMovies(
     headers: {
       Authorization: `Bearer ${TOKEN}`,
     },
-  };
-
-  const url = `${BASE_URL}/search/movie`;
-  const response = await axios.get<MoviesResponse>(url, config);
+  });
 
   return response.data;
 }
